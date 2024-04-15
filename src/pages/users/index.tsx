9abdb165fe37a8e492c9813fa367c9f635f9b817
Himanshu <xylogramori@gmail.com>
2024-04-16 00:46:38 +0530

    users Page functionalities - add & fetchalluser

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import AddUser from "../../components/addUser/AddUser";
-
+import { Link } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
 const UsersPage = () => {
-    const [openAdd , setOpenAdd]=useState(false);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [isError, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_API_URL + "/user/add/",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  },[]);
+
   return (
     <>
       <Navbar />
@@ -11,7 +45,6 @@ const UsersPage = () => {
         <div className="col-span-2">
           <div className="my-4 max-w-md mx-auto">
             <div className="">
-              
               <input
                 type="search"
                 id="default-search"
@@ -21,11 +54,18 @@ const UsersPage = () => {
               />
             </div>
           </div>
-         {!openAdd?
-        <button className="w-[100%] bg-blue-500 text-white py-1 rounded-md" onClick={()=>{setOpenAdd(!openAdd)}}>ADD NEW USER</button> :<AddUser/> 
-        }
-                 
-          
+          {!openAdd ? (
+            <button
+              className="w-[100%] bg-blue-500 text-white py-1 rounded-md"
+              onClick={() => {
+                setOpenAdd(!openAdd);
+              }}
+            >
+              ADD NEW USER
+            </button>
+          ) : (
+            <AddUser />
+          )}
         </div>
 
         <div className=" col-span-5 m-4 rounded-md overflow-x-auto shadow-md sm:rounded-lg">
@@ -33,7 +73,7 @@ const UsersPage = () => {
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">
-                  User name
+                  Name
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Roll No
@@ -42,80 +82,44 @@ const UsersPage = () => {
                   Branch
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Enrolled on
+                  Status
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Action
                 </th>
               </tr>
             </thead>
-            <tbody>
-              <tr className="odd:bg-white  odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  User Name-1
-                </th>
-                <td className="px-6 py-4">21bec000</td>
-                <td className="px-6 py-4">ECE</td>
-                <td className="px-6 py-4">22-1-2024</td>
-                <td className="px-6 flex gap-1 py-4">
-                  <button className="bg-blue-500 py-1 px-2 rounded-md text-white">
-                    Edit
-                  </button>
-                  <button className="bg-green-500 py-1 px-2 rounded-md text-white">
-                    View
-                  </button>
-                  <button className="bg-red-500 py-1 px-2 rounded-md text-white">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr className="odd:bg-white  odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  User Name-1
-                </th>
-                <td className="px-6 py-4">21bec000</td>
-                <td className="px-6 py-4">ECE</td>
-                <td className="px-6 py-4">22-1-2024</td>
-                <td className="px-6 flex gap-1 py-4">
-                  <button className="bg-blue-500 py-1 px-2 rounded-md  text-white">
-                    Edit
-                  </button>
-                  <button className="bg-green-500 py-1 px-2 rounded-md text-white">
-                    View
-                  </button>
-                  <button className="bg-red-500 py-1 px-2 rounded-md text-white">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr className="odd:bg-white  odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  User Name-1
-                </th>
-                <td className="px-6 py-4">21bec000</td>
-                <td className="px-6 py-4">ECE</td>
-                <td className="px-6 py-4">22-1-2024</td>
-                <td className="px-6 flex gap-1 py-4">
-                  <button className="bg-blue-500 py-1 px-2 rounded-md text-white">
-                    Edit
-                  </button>
-                  <button className="bg-green-500 py-1 px-2 rounded-md text-white">
-                    View
-                  </button>
-                  <button className="bg-red-500 py-1 px-2 rounded-md text-white">
-                    Delete
-                  </button>
-                </td>
-              </tr>
+            <tbody >
+              {loading ? (
+                <div className="w-[100%]"><Loader  text={"Fetching History"}/></div>
+              ) : (
+                users.map((user) => (
+                  <tr
+                    key={user.email}
+                    className="odd:bg-white  odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {user.name}
+                    </th>
+                    <td className="px-6 py-4">{user.roll_no}</td>
+                    <td className="px-6 py-4">{user.email}</td>
+                    <td className="px-6 py-4">
+                      {user.is_active ? "ACTIVE" : "INACTIVE"}
+                    </td>
+                    <td className="px-6 flex gap-1 py-4">
+                      <button className="bg-green-500 py-1 px-2 rounded-md text-white">
+                        <Link to={`/users/${user.roll_no}`}>VIEW</Link>
+                      </button>
+                      <button className="bg-red-500 py-1 px-2 rounded-md text-white">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
